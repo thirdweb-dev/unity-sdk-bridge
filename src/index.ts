19afed98ef727da2da42a02591a7e9476a093551
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-03-10 01:36:56 +0000

    sendmessage override

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import { ISDKOptions, ThirdwebSDK, ValidProviderInput } from "@3rdweb/sdk";
 import { v4 } from "uuid";
 
+type SendResultFunction = (
+  route: string,
+  request: string,
+  response: string
+) => void;
+
 export class ThirdwebBridgeSDK extends ThirdwebSDK {
   constructor(
     providerOrNetwork: ValidProviderInput,
-    opts?: Partial<ISDKOptions>
+    opts?: Partial<ISDKOptions>,
+    resultFunc?: SendResultFunction
   ) {
     super(providerOrNetwork, opts);
 
     // initialize thirdweb bridge sdk
     const w = window as any;
     w.bridge = {};
+    w.bridge.sendResult = resultFunc;
     w.bridge.invoke = (route: string, payload: string) => {
       console.log("invoke called", route, payload);
       const ack_id = v4();
@@ -19,9 +27,12 @@ export class ThirdwebBridgeSDK extends ThirdwebSDK {
         console.log("invoke route result", result);
         const returnPayload = JSON.stringify({ ack_id, result });
 
-        // note: can let user pass in any SendMessage
-        // for now this only work with our unity sdk
-        w.unityInstance.SendMessage("Thirdweb", "Callback", returnPayload);
+        if (w.bridge.sendResult) {
+          w.bridge.sendResult(route, payload, returnPayload);
+        } else {
+          // by default, we send the result back to the bridge through window.unityInstance (backward compatibility)
+          w.unityInstance.SendMessage("Thirdweb", "Callback", returnPayload);
+        }
       });
       return ack_id;
     };
